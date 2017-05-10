103e0476ec73143f36b8886aa0cb1a1188bd7c45
Ilja <ilja.goethel@web.de>
2017-05-10 16:38:10 +0200

    Aenderung der urls rueckgebaut

--- Scholien/urls.py
+++ Scholien/urls.py
@@ -9,7 +9,7 @@ app_name = 'Scholien'
 urlpatterns = [
     url(r'^$', liste_artikel, name='index'), 
     url(r'^buechlein/$', liste_buechlein, name='buechlein_liste'),     
-    url(r'^(?P<slug>[-\w]+)/$', ein_artikel, name='artikel_detail'),
+    url(r'^/(?P<slug>[-\w]+)/$', ein_artikel, name='artikel_detail'),
     url(r'^aus_datei_einlesen$', 
         views.daten_einlesen, 
         name='aus_datei_einlesen'), 
--- Veranstaltungen/urls.py
+++ Veranstaltungen/urls.py
@@ -22,7 +22,7 @@ salons_urls = ([
     url(r'^s', 
         views.liste_veranstaltungen, 
         {'art': 'Salon'}, name='liste_salons'),
-    url(r'^(?P<slug>[-\w]+)/$', 
+    url(r'^/(?P<slug>[-\w]+)/$', 
         views.VeranstaltungDetail.as_view(), 
         {'art': 'Salon'}, name='salon_detail'),
     ], 'Veranstaltungen')
@@ -31,7 +31,7 @@ seminare_urls = ([
     url(r'^e', 
         views.liste_veranstaltungen, 
         {'art': 'Seminar'}, name='liste_seminare'),
-    url(r'^(?P<slug>[-\w]+)/$', 
+    url(r'^/(?P<slug>[-\w]+)/$', 
         views.VeranstaltungDetail.as_view(), 
         {'art': 'Seminar'}, name='seminar_detail'),
     ], 'Veranstaltungen')
