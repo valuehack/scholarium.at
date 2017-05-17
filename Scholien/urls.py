f2bf2275b8f7762b50029b149a4c17a850f6171f
geraldk1 <gk@scholarium.at>
2017-05-17 16:54:12 +0200

    scholienbüchlein detail: zwischenstand

--- Scholien/urls.py
+++ Scholien/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     url(r'^/$', liste_artikel, name='index'), 
     url(r'^buechlein/$', liste_buechlein, name='buechlein_liste'),     
     url(r'^/(?P<slug>[-\w]+)/$', ein_artikel, name='artikel_detail'),
+    url(r'^buechlein/(?P<slug>[-\w]+)/$', ein_buechlein, name='buechlein_detail'),
     url(r'^aus_datei_einlesen$', 
         views.daten_einlesen, 
         name='aus_datei_einlesen'), 
