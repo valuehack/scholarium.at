2627d52abc5fb922a6d20db101d24e21cc00ce99
Ilja <ilja.goethel@web.de>
2017-09-04 01:31:44 +0200

    daten_einlesen guckt, ob daten schon da sind vor dem Einlesen

--- Grundgeruest/daten_einlesen.py
+++ Grundgeruest/daten_einlesen.py
@@ -9,24 +9,30 @@ def buechlein_ebooks_einlesen():
     """ Kopiert die pdfs/epub/mobi der scholienbuechlein aus dem 
     input-Ordner nach media, verändert sicherheitshalber den Dateinamen, 
     und speichert den link in der db """
-    liste = Buechlein.objects.all()[2:4]
-    namen = [b.slug+ext for b in liste for ext in ['.pdf', '.mobi', '.epub']]
+    liste = Buechlein.objects.all()
+    buechlein = [(b.slug, b, [b.slug+ext for ext in ['.pdf', '.mobi', '.epub']]) for b in liste]
     
     os.chdir('/home/scholarium/')
     neue_namen = {}
-    for name in namen:
-        von = 'production/down_secure/content_secure/' + name
-        n, ext = os.path.splitext(name)
-        neu = n + '_' + Nutzer.erzeuge_zufall(8, 2) + ext
-        neue_namen.update([(name, neu)])
-        nach = os.path.join(MEDIA_ROOT, 'scholienbuechlein', neu)
-        shutil.copy2(von, nach)
+    for slug, b, dreinamen in buechlein:
+        for name in dreinamen:
+            von = 'production/down_secure/content_secure/' + name
+            n, ext = os.path.splitext(name)
+            nachordner = os.path.join(MEDIA_ROOT, 'scholienbuechlein')
+            dateifeld = getattr(b, ext)
+            if dateifeld and dateifeld.name in os.listdir(nachordner):
+                neue_namen.update([(name, dateifeld.name)])
+                continue
+            else:
+                neu = n + '_' + Nutzer.erzeuge_zufall(8, 2) + ext
+                neue_namen.update([(name, neu)])
+                nach = nachordner + neu
+                shutil.copy2(von, nach)
+                dateifeld.name = 'scholienbuechlein/' + neu
 
-    for b in liste:
-        b.pdf.name = 'scholienbuechlein/' + neue_namen[b.slug+'.pdf']    
-        b.mobi.name = 'scholienbuechlein/' + neue_namen[b.slug+'.mobi']    
-        b.epub.name = 'scholienbuechlein/' + neue_namen[b.slug+'.epub']    
-        bild = b.slug + '.jpg'
-        b.bild_holen('http://www.scholarium.at/schriften/'+bild, bild)
+        if not b.bild:
+            bild = b.slug + '.jpg'
+            b.bild_holen('http://www.scholarium.at/schriften/'+bild, bild)
+        
         b.save()
         
