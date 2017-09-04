433a7b5a82a0dd703137f1cf36cd7d862a7378c6
ilja <ilja.goethel@arcor.de>
2017-09-04 11:41:44 +0200

    Zwischenstand

--- Grundgeruest/daten_einlesen.py
+++ Grundgeruest/daten_einlesen.py
@@ -24,6 +24,8 @@ def buechlein_ebooks_einlesen():
                 name_neu = '%s_%s.%s' % (b.slug, Nutzer.erzeuge_zufall(8, 2), ext)
                 dateifeld.name = 'scholienbuechlein/%s' % name_neu
                 shutil.copy2(von, os.path.join(MEDIA_ROOT, dateifeld.name)
+        
+        b.save()
 
         if not b.bild:
             bild = b.slug + '.jpg'
@@ -31,3 +33,73 @@ def buechlein_ebooks_einlesen():
         
         b.save()
         
+
+def veranstaltungen_aus_db():
+    """ liest veranstaltungen aus alter db (als .sqlite exportiert) aus 
+    !! Achtung, löscht davor die aktuellen Einträge !! """
+    
+    from django.db import transaction
+    import sqlite3 as lite
+    from django.conf import settings
+    import os, pdb
+    from django.http import HttpResponseRedirect
+    
+    # Seminare, Salons, Vorlesungen, Vorträge einlesen
+    Veranstaltung.objects.all().delete()
+    
+    con = lite.connect(os.path.join(settings.BASE_DIR, 'alte_db.sqlite3'))
+    with con:
+        con.row_factory = lite.Row
+        cur = con.cursor()
+        cur.execute("SELECT * FROM produkte WHERE type in ('salon'," + 
+            " 'seminar', 'media-vortrag', 'media-vorlesung');")
+
+        zeilen = [dict(zeile) for zeile in cur.fetchall()]
+    
+    arten = {'salon': ArtDerVeranstaltung.objects.get(
+            bezeichnung='Salon'),
+        'seminar': ArtDerVeranstaltung.objects.get(
+            bezeichnung='Seminar'),
+        'media-vortrag': ArtDerVeranstaltung.objects.get(
+            bezeichnung='Vortrag'),
+        'media-vorlesung': ArtDerVeranstaltung.objects.get(
+            bezeichnung='Vorlesung'),}
+    
+    with transaction.atomic():
+        for zeile in zeilen:
+            if zeile['type'] in ['seminar', 'salon']:
+                datum = zeile['start'] or '1111-01-01 00-00'
+            else:
+                if zeile['last_donation'] in ['0000-00-00 00:00:00', None]:
+                    datum = '1111-01-01 00-00'
+                else:
+                    datum = zeile['last_donation']
+                    
+            datum = datum.split(' ')[0]
+            v = Veranstaltung.objects.create(
+                bezeichnung=zeile['title'],
+                slug=zeile['id'],
+                beschreibung=zeile['text'],
+                art_veranstaltung=arten[zeile['type']],
+                datum=datum, 
+                link=zeile['livestream'])
+
+    # Studiendinger einlesen
+    Studiumdings.objects.all().delete()
+    
+    con = lite.connect(os.path.join(settings.BASE_DIR, 'alte_db.sqlite3'))
+    with con:
+        con.row_factory = lite.Row
+        cur = con.cursor()
+        cur.execute("SELECT * FROM produkte WHERE type='programm';")
+
+        zeilen = [dict(zeile) for zeile in cur.fetchall()]
+        
+    with transaction.atomic():
+        for zeile in zeilen:
+            dings = Studiumdings.objects.create(
+                bezeichnung=zeile['title'],
+                slug=zeile['id'],
+                beschreibung1=zeile['text'],
+                beschreibung2=zeile['text2'],
+                preis_teilnahme=zeile['price'],)
--- Veranstaltungen/views.py
+++ Veranstaltungen/views.py
@@ -108,79 +108,12 @@ def vortrag(request):
         template_name='Gast/vortrag.html', 
         extra_context=extra_context)(request)
 
-    
+
+from Grundgeruest.daten_einlesen import veranstaltungen_aus_db
 def daten_einlesen(request):
     """ wird von url aufgerufen und ruft standalone-Fkt auf """
     aus_alter_db_einlesen()
     return HttpResponseRedirect('/veranstaltungen')
     
 
-def aus_alter_db_einlesen():
-    """ liest veranstaltungen aus alter db (als .sqlite exportiert) aus 
-    !! Achtung, löscht davor die aktuellen Einträge !! """
-    
-    from django.db import transaction
-    import sqlite3 as lite
-    from django.conf import settings
-    import os, pdb
-    from django.http import HttpResponseRedirect
-    
-    # Seminare, Salons, Vorlesungen, Vorträge einlesen
-    Veranstaltung.objects.all().delete()
-    
-    con = lite.connect(os.path.join(settings.BASE_DIR, 'alte_db.sqlite3'))
-    with con:
-        con.row_factory = lite.Row
-        cur = con.cursor()
-        cur.execute("SELECT * FROM produkte WHERE type in ('salon'," + 
-            " 'seminar', 'media-vortrag', 'media-vorlesung');")
 
-        zeilen = [dict(zeile) for zeile in cur.fetchall()]
-    
-    arten = {'salon': ArtDerVeranstaltung.objects.get(
-            bezeichnung='Salon'),
-        'seminar': ArtDerVeranstaltung.objects.get(
-            bezeichnung='Seminar'),
-        'media-vortrag': ArtDerVeranstaltung.objects.get(
-            bezeichnung='Vortrag'),
-        'media-vorlesung': ArtDerVeranstaltung.objects.get(
-            bezeichnung='Vorlesung'),}
-    
-    with transaction.atomic():
-        for zeile in zeilen:
-            if zeile['type'] in ['seminar', 'salon']:
-                datum = zeile['start'] or '1111-01-01 00-00'
-            else:
-                if zeile['last_donation'] in ['0000-00-00 00:00:00', None]:
-                    datum = '1111-01-01 00-00'
-                else:
-                    datum = zeile['last_donation']
-                    
-            datum = datum.split(' ')[0]
-            v = Veranstaltung.objects.create(
-                bezeichnung=zeile['title'],
-                slug=zeile['id'],
-                beschreibung=zeile['text'],
-                art_veranstaltung=arten[zeile['type']],
-                datum=datum, 
-                link=zeile['livestream'])
-
-    # Studiendinger einlesen
-    Studiumdings.objects.all().delete()
-    
-    con = lite.connect(os.path.join(settings.BASE_DIR, 'alte_db.sqlite3'))
-    with con:
-        con.row_factory = lite.Row
-        cur = con.cursor()
-        cur.execute("SELECT * FROM produkte WHERE type='programm';")
-
-        zeilen = [dict(zeile) for zeile in cur.fetchall()]
-        
-    with transaction.atomic():
-        for zeile in zeilen:
-            dings = Studiumdings.objects.create(
-                bezeichnung=zeile['title'],
-                slug=zeile['id'],
-                beschreibung1=zeile['text'],
-                beschreibung2=zeile['text2'],
-                preis_teilnahme=zeile['price'],)
