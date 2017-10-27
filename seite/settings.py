70fae439319dbe21ea566c9aae19691c373f892f
scholarium <info@scholarium.at>
2017-10-27 04:33:39 +0200

    Dank-Mails bei Unterstuetzung - ilja im CC rausgenommen; cc-Empfaenger ist info; das steht in settings, damit's in staging anders ist

--- seite/settings.py
+++ seite/settings.py
@@ -218,6 +218,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'iljasseite@gmail.com'
 EMAIL_HOST_PASSWORD = 'iljailja'
+DEFAULT_TO_EMAILS = ['mb@scholarium.at', 'ilja1988@gmail.com']
 
 # Paypal
 PAYPAL_MODE = 'sandbox'   # sandbox or live
