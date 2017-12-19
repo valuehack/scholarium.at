709ebe22952ca1bfe09f38da27d17fb03d7c2083
geraldk1 <gk@scholarium.at>
2017-12-19 21:14:17 +0000

    Workflow/urls: add trailing slashes

--- Workflow/urls.py
+++ Workflow/urls.py
@@ -5,6 +5,6 @@ from .views import control_view, rechnung_view
 app_name = 'Workflow'
 
 urlpatterns = [
-    url('^skripte$', control_view, name='skripte'),
-    url('^rechnungen$', rechnung_view, name='rechnungen'),
+    url('^skripte/$', control_view, name='skripte'),
+    url('^rechnungen/$', rechnung_view, name='rechnungen'),
 ]
