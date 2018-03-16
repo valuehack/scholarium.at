049b345dc1009a146c122f0dba14ef2a5751fb6f
MerlinB <merlin.buczek@gmail.com>
2018-03-16 15:22:55 +0100

    Generalized export function

--- Workflow/urls.py
+++ Workflow/urls.py
@@ -1,12 +1,11 @@
 from django.conf.urls import url
 
-from .views import control_view, skripte_view, rechnung_view, csv_export
+from .views import control_view, skripte_view, rechnung_view
 
 app_name = 'Workflow'
 
 urlpatterns = [
     url('^skripte/$', skripte_view, name='skripte'),
     url('^rechnungen/$', rechnung_view, name='rechnungen'),
-    url(r'^csv/(?P<value>[\w]+)/$', csv_export, name='csv'),
     url(r'^$', control_view, name='index'),
 ]
