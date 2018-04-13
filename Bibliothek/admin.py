36b66c2484efb8196fcfe0505ce06c51aed2c716
Merlin Buczek <merlin.buczek@gmail.com>
2018-04-13 10:37:46 +0200

    Admin Improvements

--- Bibliothek/admin.py
+++ Bibliothek/admin.py
@@ -5,6 +5,6 @@ from .models import Buch
 
 @admin.register(Buch)
 class BuchAdmin(admin.ModelAdmin):
-    list_display = ('autor', 'titel', 'herausgeber', 'bezeichnung')
+    list_display = ('autor', 'titel', 'herausgeber', 'bezeichnung', 'ob_pdf', 'ob_mobi', 'ob_epub')
     list_filter = ['jahr']
     search_fields = ['bezeichnung']
