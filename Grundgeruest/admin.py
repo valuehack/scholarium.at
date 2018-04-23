610cbc8ea0e6a66932dec5755acad54a7444c55d
Merlin Buczek <merlin.buczek@gmail.com>
2018-04-23 14:04:27 +0200

    admin search fix

--- Grundgeruest/admin.py
+++ Grundgeruest/admin.py
@@ -32,7 +32,7 @@ class StufenPlusUnterstuetzerListFilter(admin.SimpleListFilter):  # Nicht in Ben
 
 class ProfileAdmin(admin.ModelAdmin):
     list_filter = ['land']
-    search_fields = ['user__email', 'user']
+    search_fields = ['user__email', 'user__first_name', 'user__last_name']
     list_display = ['anrede', 'user', 'guthaben', 'get_Status', 'get_aktiv', 'get_ablauf']
 
 
